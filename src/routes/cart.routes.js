be8bf6a29827c6d79b5a47b2e8e0bac487af39bc
NalvinCode <mariano.alvarez.personal@outlook.com>
2025-02-17 02:06:33 -0300

    cambios actualizados Thiago

--- src/routes/cart.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { addToCart, getCart } = require("../controllers/cartController");
-
-const router = express.Router();
-
-router.post("/", addToCart);
-router.get("/:usuario_id", getCart);
-
-module.exports = router;
